5c24e1517a99f3d59cae1aab810c6f6c00b49f0c
Júlio César Damásio Dos Santos <damaziojulio@gmail.com>
2026-01-20 13:00:38 -0300

    Integração via API com o SIAFI WEB

--- Api.py
+++ Api.py
@@ -1,33 +1,28 @@
 import requests
+import os
 
-# Endpoint de homologação
-url = "https://homextservicos-siafi.tesouro.gov.br/siafi2026he/services/tabelas/consultarTabelasAdministrativas?wsdl"
+CPF = "10628518625"
+SENHA = os.getenv("SIAFI_SENHA")  # ideal
 
-response = requests.get(
-    url,
-    cert=r"C:\Users\juliodamasio\Documents\meu_certificado.pfx",
-    timeout=30
-)
+# Endpoint
+url = "https://servicos-siafi.tesouro.gov.br/siafi2026/services/tabelas/consultarTabelasAdministrativas"
 
-print(response.status_code)
+# Caminhos dos certificados
+CERT_CLIENTE = r"W:\B - TED\7 - AUTOMAÇÃO\Scripts\siafi\certificado\wssimecpfmecgovbr.pem"
+CA_SIAFI     = r"W:\B - TED\7 - AUTOMAÇÃO\Scripts\siafi\certificado\cadeiaCA.pem"
 
-# Certificado (arquivo .pem e .key)
-cert = (
-    
-)
-
-# Headers HTTP
+# Headers
 headers = {
     "Content-Type": "text/xml; charset=utf-8",
     "SOAPAction": ""
 }
 
-# Envelope SOAP (SEU XML, sem alterações estruturais)
-soap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+# Envelope SOAP
+soap_xml = soap_xml = soap_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
    <soap:Header>
       <ns2:cabecalhoSIAFI xmlns:ns2="http://www.tesouro.gov.br/siafi/services/tabelas/consultarTabelasAdministrativas">
-         <ug>173057</ug>
+         <ug>152734</ug>
          <bilhetador>
             <nonce>144966</nonce>
          </bilhetador>
@@ -35,9 +30,9 @@ soap_xml = """<?xml version="1.0" encoding="UTF-8"?>
 
       <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
          <wsse:UsernameToken>
-            <wsse:Username>10628518625</wsse:Username>
+            <wsse:Username>{CPF}</wsse:Username>
             <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">
-            
+               {SENHA}
             </wsse:Password>
          </wsse:UsernameToken>
       </wsse:Security>
@@ -46,26 +41,29 @@ soap_xml = """<?xml version="1.0" encoding="UTF-8"?>
    <soap:Body>
       <ns2:tabConsultarSaldoContabil xmlns:ns2="http://www.tesouro.gov.br/siafi/services/tabelas/consultarTabelasAdministrativas">
          <tabConsultarSaldo>
-            <codUG></codUG>
-            <contaContabil></contaContabil>
-            <contaCorrente></contaCorrente>
+            <codUG>152734</codUG>
+            <contaContabil>622110000</contaContabil>
+            <mesRefSaldo>JAN</mesRefSaldo>
+            <outrosParamSaldoContabil>
+               <codFonteRec>1000A0008U</codFonteRec>
+               <codPtres>229566</codPtres>
+            </outrosParamSaldoContabil>   
          </tabConsultarSaldo>
       </ns2:tabConsultarSaldoContabil>
    </soap:Body>
 </soap:Envelope>
 """
 
-# Requisição
+# Requisição SOAP com mTLS
 response = requests.post(
     url,
     data=soap_xml.encode("utf-8"),
     headers=headers,
-    cert=cert,
-    verify=True,  # valida SSL
-    timeout=30
+    cert=CERT_CLIENTE,   # <<< certificado cliente (mTLS)
+    verify=CA_SIAFI,     # <<< CA do SIAFI 
+    timeout=60
 )
 
-# Resultado
 print("Status HTTP:", response.status_code)
 print("Resposta SOAP:")
 print(response.text)
